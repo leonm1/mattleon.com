9233a801a0d1fea014cfe2ff2faa76d6314ff79d
Matthew Leon <32306579+leonm1@users.noreply.github.com>
2017-10-01 14:08:47 -0500

    Finished fun.js
    Added division and cubeNumber functions

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
-console.log("Hey!");
+console.log("Hey Matthew!");
 
 // you define variables with let, a variable name and then a value
 let foo = 2;
@@ -8,15 +8,17 @@ console.log(foo);
 let bar;
 console.log("Bar is: " + bar); // should be undefined
 
-bar =3;
+bar = 3;
 console.log("Bar is now: " + bar); // should be 3
 
 
 // let's try multiplying
-console.log(foo + '*' + bar + '=' +(foo*bar));
+console.log(foo + ' * ' + bar + ' = ' + (foo * bar));
 
 // now you do division!
-console.log(foo + '*' + bar + '=' +('ADD A DIVISION EXPRESSION HERE'));
+console.log(foo + ' / ' + bar + ' = ' + (foo / bar));
+
+printCubeNumber(bar);
 
 
 
@@ -25,7 +27,7 @@ function squareNumber(number){ // square a number
     return Math.pow(number,2); // return is what the function ends up being
 }
 
-console.log(squareNumber(bar)); // should be 9
+console.log('3 squared is: ' + squareNumber(20)); // should be 9
 
 
 function printSquareNumber(number){ // calls square number and prints the results
@@ -37,11 +39,11 @@ function printSquareNumber(number){ // calls square number and prints the result
 // call printSquareNumber
 
 function cubeNumber(number){
-    //write this with a return statement (like squareNumber)
+    return Math.pow(number,3);
 }
 
 function printCubeNumber(number){
-    // write this with a console.log() statement!
+    console.log(number + ' cubed is: ' + cubeNumber(number));
 }
 
 
